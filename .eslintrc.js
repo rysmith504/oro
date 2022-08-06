3f9a263cb734a15714244b965ee1f4b1ca0d40e5
Bethany Jones <betpetjones@gmail.com>
2022-08-06 15:28:03 -0500

    update eslint and start scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,43 @@
-{
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    es2021: true,
+    node: true,
   },
-  "plugins": [
+  parserOptions: {
+  ecmaVersion: 2022,
+  sourceType: "module"
+  },
+  ignorePatterns: [
+    '.eslint*',
+    '**/dist/',
+    '**/node_modules'
+  ],
+  plugins: [
     "@typescript-eslint",
     "react-hooks"
   ],
-  "extends": [
+  extends: [
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended"
   ],
-  "rules": {
+  rules: {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
     "react/prop-types": "off"
   },
-  "settings": {
-    "react": {
-      "pragma": "React",
-      "version": "detect"
+  settings: {
+    react: {
+      pragma: "React",
+      version: "detect"
     }
   }
 }
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const App: React.FC = () => {
+
   return <h1>Oro App </h1>;
 };
 
