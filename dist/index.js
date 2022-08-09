d3320152bc739f052dc6a99bbab34728f2f129a8
Bethany Jones <betpetjones@gmail.com>
2022-08-09 18:52:20 -0500

    add component files

--- dist/index.js
+++ dist/index.js
@@ -3,17 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
 var path_1 = __importDefault(require("path"));
+var express_1 = __importDefault(require("express"));
 var app = (0, express_1.default)();
-app.use(express_1.default.static(path_1.default.join(__dirname, "../public")));
-app.get("/", function (req, res, next) {
-    try {
-        res.send("index.html");
-    }
-    catch (error) {
-    }
-});
+app.use(express_1.default.static(path_1.default.join(__dirname, '../public')));
 var PORT = 5000;
 app.listen(PORT, function () {
     console.log("App listening on port http://localhost:".concat(PORT));
