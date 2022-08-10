3120195cc34c2a41b0f88ac0de9096a86bd32d63
dow2 <dowedwards@gmail.com>
2022-08-10 10:47:02 -0500

    user context boilerplate

--- client/index.tsx
+++ client/index.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import App from './components/App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
+import { UserContextProvider } from './context/userContext';
 const rootElement = document.getElementById('root');
 
-if (!rootElement) { throw new Error('Failed to find the root element'); }
+if (!rootElement) {
+  throw new Error('Failed to find the root element');
+}
 const root = createRoot(rootElement);
 
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="*" element={<App/>}/>
-    </Routes>
-  </BrowserRouter>
+  <UserContextProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path='*' element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </UserContextProvider>
 );
