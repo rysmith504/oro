5c94765fc7b6b198efa0f4b4e41e546c2cf302c9
dow2 <dowedwards@gmail.com>
2022-08-10 19:22:29 -0500

    console logs removed

--- server/routes/eventDetail.ts
+++ server/routes/eventDetail.ts
@@ -4,14 +4,12 @@ import { Router } from 'express';
 const eventDetailsRouter = Router();
 eventDetailsRouter.get('/', (req, res) => {
   const { id } = req.query;
-  console.log('in eventDetails');
   axios
     .get(
       `https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=${process.env.TICKETMASTER_API_KEY}&id=${id}`
     )
     .then((results) => {
       const { data } = results;
-      console.log(data);
       res.status(200).send(data);
     })
     .catch((error) => console.error(error));
