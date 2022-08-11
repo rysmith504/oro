fcd2292a1d0ca15a9bb157061defcf032860c881
rysmith504 <rysmith504@gmail.com>
2022-08-11 01:12:28 -0500

    added card to eventlistingstsx, added some more functionality to get req for events

--- server/routes/eventListingsRouter.ts
+++ server/routes/eventListingsRouter.ts
@@ -10,13 +10,28 @@ const eventListingsRouter = Router();
 console.log('hello');
 
 eventListingsRouter.get('/list', (req, res) => {
-  console.log(req.query)
+  // console.log(req.query)
   // const punctuationless = req.query.keyword
   // .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, " ")
   // .replace(/\s{1,}/g, "+")
   // .toLowerCase();
-  axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=${process.env.TICKETMASTER_API_KEY}`)
-    .then((responseObj) => console.log(responseObj))
+  const { keyword } = req.query;
+  console.log('KEYWROD',keyword);
+  axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?size=10&keyword=${keyword}&apikey=${process.env.TICKETMASTER_API_KEY}`)
+    .then((responseObj) => {
+      const dates = responseObj.data._embedded.events.filter((event) => {
+        return event.dates.start
+      }).map(event => {
+        return ([event.id, event.dates.start])
+      })
+      console.log('DATES12' , dates);
+      console.log('RESPONSE OBJ GET', responseObj.data._embedded)
+      res.send({
+        
+      })
+      res.sendStatus(200);
+    })
+    .catch(err => console.error(err));
 })
 
 
