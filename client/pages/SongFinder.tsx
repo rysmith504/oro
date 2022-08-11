bf0196a85d4c9b227b4948c03bdfc40da2ff3697
Vincent Tong <vincent.khang.tong@gmail.com>
2022-08-11 18:59:22 -0500

    update song recognition works but calls twice?

--- client/pages/SongFinder.tsx
+++ client/pages/SongFinder.tsx
@@ -6,9 +6,7 @@ const Mp3Recorder = new MicRecorder({ bitRate: 128});
 const SongFinder: React.FC = () => {
 
   const [isRecording, setIsRecording] = useState(false);
-  const [blobURL, setBlobURL] = useState('');
   const [isBlocked, setIsBlocked] = useState(false);
-  const [blob, setBlob] = useState();
   const [previewSource, setPreviewSource] = useState();
 
   useEffect(() => {
@@ -48,50 +46,16 @@ const SongFinder: React.FC = () => {
   const stop = () => {
     Mp3Recorder.stop().getMp3()
       .then(([buffer, blob]) => {
-        // console.log(blob);
-        // const newBlob = new Blob([buffer], {type: 'audio/mp3'});
-        // const url = URL.createObjectURL(newBlob);
-        // console.log(url);
-        // const blobURL = URL.createObjectURL(blob);
-
-        // const wavfromblob = new File([blob], 'incomingaudioclip.wav');
-        // console.log('WAV FILE', wavfromblob);
-        // setBlob(blob);
         const reader = new FileReader();
         reader.readAsDataURL(blob);
         reader.onloadend = async () => {
           setPreviewSource(reader.result);
-
-
-          // console.log(previewSource);
         };
-        // setBlobURL(blobURL);
         setIsRecording(false);
-
-        
       })
       .catch((e) => console.log(e));
   };
 
-
-
-  const sendAudioFile = () => {
-    // const form = new FormData();
-    // form.append("file", file, 'incomingAudio');
-    // console.log('FORM', form);
-    setBlobURL('dlkfadsfkla');
-
-  };
-
-  // const fileData = () => {
-  //   console.log('BLOB', blob);
-
-  //   if (blob) {
-
-  //       console.log(previewSource);
-  //     };
-  //   }
-  // };
 
 
 
@@ -103,7 +67,6 @@ const SongFinder: React.FC = () => {
       <button onClick={start} disabled={isRecording}>RECORD</button>
       <button onClick={stop} disabled={!isRecording}>STOP</button>
       <audio src={previewSource} controls="controls"/>
-      <button onClick={() => sendAudioFile()}>SEND TO API</button>
     </div>
   );
 };
--- server/routes/songFinder.ts
+++ server/routes/songFinder.ts
@@ -3,10 +3,8 @@ import axios from 'axios';
 import path from 'path';
 // import dotenv from 'dotenv';
 require('dotenv').config();
-// import cloudinary from 'cloudinary';
 import { v2 as cloudinary } from 'cloudinary';
 
-// dotenv.config({path: path.resolve(__dirname, '../../.env')});
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
@@ -23,20 +21,28 @@ songFinderRouter.post('/', async (req, res) => {
   try {
     // console.log(req.body.data);
     const fileStr = req.body.data;
-    const uploadedResponse = await cloudinary.uploader.upload(fileStr, {resource_type: 'video', upload_preset: 'trailfeathers'})
-      .then((data) => {
-        console.log(data);
-        res.status(200).send(data);
+    await cloudinary.uploader.upload(fileStr, {resource_type: 'video', upload_preset: 'VibeSocietyAudio'})
+      .then((uploadResponse) => {
+        // console.log(uploadResponse);
+        axios.post('https://api.audd.io/', {
+      
+          'api_token': process.env.AUDD_TOKEN,
+          'url': uploadResponse.secure_url,
+          'return': 'apple_music,spotify',
+          
+        })
+          .then((data) => {
+            console.log(data.data)
+            res.status(200).send(data);
+          })
+          .catch((err) => res.sendStatus(500));
       })
       .catch((err) => res.sendStatus(500));
   } catch (error) {
     res.sendStatus(500);
   }
 });
-    // .then(() => res.status(200).send(uploadedResponse))
-    // .catch(() => res.sendStatus(500));
-  // console.log(req.body.data);
-  // res.send(req.body);=
+
 
 
 export default songFinderRouter;
