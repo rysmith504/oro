f71e29d5abf6699ddbb901a79a94654bd4039cd3
Bethany Jones <betpetjones@gmail.com>
2022-08-11 19:37:18 -0500

    merged

--- dist/routes/eventListingsRouter.js
+++ dist/routes/eventListingsRouter.js
@@ -8,24 +8,48 @@ var express_1 = require("express");
 require('dotenv').config();
 var eventListingsRouter = (0, express_1.Router)();
 eventListingsRouter.get('/list', function (req, res) {
-    // console.log(req.query)
-    // const punctuationless = req.query.keyword
-    // .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, " ")
-    // .replace(/\s{1,}/g, "+")
-    // .toLowerCase();
     var keyword = req.query.keyword;
-    // console.log('KEYWROD',keyword);
     axios_1.default.get("https://app.ticketmaster.com/discovery/v2/events.json?size=10&keyword=".concat(keyword, "&apikey=").concat(process.env.TICKETMASTER_API_KEY))
         .then(function (responseObj) {
-        var dates = responseObj.data._embedded.events.filter(function (event) {
-            return event.dates.start;
-        }).map(function (event) {
-            return ([event.id, event.dates.start]);
+        var venueInfo;
+        var events = responseObj.data._embedded.events.filter(function (event) {
+            return event._embedded;
+        }).map(function (event, count) {
+            var newDataObj = {
+                eventDate: event.dates.start.dateTime,
+                eventId: event.id,
+            };
+            var artistInfo = event._embedded.attractions.map(function (attraction) {
+                var artistInfo = {
+                    artistName: attraction.name,
+                    artistId: attraction.id,
+                    artistImages: attraction.images
+                };
+                return artistInfo;
+            });
+            var venueInfo = event._embedded.venues.map(function (venue) {
+                var venueInfo = {
+                    venueId: venue.id,
+                    venueName: venue.name,
+                    address: venue.address,
+                    city: venue.city.name,
+                    state: venue.state.name,
+                    country: venue.country.name,
+                    stateCode: venue.stateCode,
+                    postalCode: venue.postalCode,
+                    location: venue.location,
+                    venueImages: venue.images
+                };
+                return venueInfo;
+            });
+            newDataObj.venueInfo = venueInfo;
+            newDataObj.artistInfo = artistInfo;
+            return newDataObj;
         });
-        // console.log('DATES12' , dates);
-        // console.log('RESPONSE OBJ GET', responseObj.data._embedded)
-        res.send({});
-        res.sendStatus(200);
+        res.send({
+            events: events
+        });
+        res.status(200);
     })
         .catch(function (err) { return console.error(err); });
 });
