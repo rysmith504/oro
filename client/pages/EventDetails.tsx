001da783fa8b26e606d4c41fa25ccf05c004fccc
dow2 <dowedwards@gmail.com>
2022-08-12 19:23:17 -0500

    added component for details page

--- client/pages/EventDetails.tsx
+++ client/pages/EventDetails.tsx
@@ -1,12 +1,40 @@
 import React, { useContext } from 'react';
 import { EventContext } from '../context/EventContext';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+// import GitHubIcon from '@mui/icons-material/GitHub';
+// import FacebookIcon from '@mui/icons-material/Facebook';
+// import TwitterIcon from '@mui/icons-material/Twitter';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import Header from './Header';
+import MainFeaturedPost from '../components/MainFeaturedPost';
+// import Sidebar from './Sidebar';
+// import Footer from './Footer';
 
 const EventDetails: React.FC = () => {
   const { getEventDetails } = useContext(EventContext);
-
   const eventData = getEventDetails('Z7r9jZ1AdFYep');
 
-  return <div>{eventData?.name}</div>;
+  const mainFeaturedPost = {
+    title: eventData?.name,
+    description: `${eventData?.venues.name}... ${eventData?.venues.city}, ${eventData?.venues.state}`,
+    image: eventData?.image.url,
+  };
+
+  const theme = createTheme();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth='lg'>
+        <main>
+          <MainFeaturedPost post={mainFeaturedPost} />
+          <Grid container spacing={5} sx={{ mt: 3 }}></Grid>
+        </main>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default EventDetails;
