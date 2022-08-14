9eff6dd6f04cc8ad4f4c2ce0c05b65b0af500b67
dow2 <dowedwards@gmail.com>
2022-08-14 16:15:05 -0500

    attempting to pull id 2

--- client/pages/EventDetails.tsx
+++ client/pages/EventDetails.tsx
@@ -1,36 +1,70 @@
-import React, { useContext } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useEffect } from 'react';
 import { EventContext } from '../context/EventContext';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
+import { Main } from '../components/EventInfoDetails.tsx';
 // import GitHubIcon from '@mui/icons-material/GitHub';
 // import FacebookIcon from '@mui/icons-material/Facebook';
 // import TwitterIcon from '@mui/icons-material/Twitter';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-// import Header from './Header';
 import MainFeaturedPost from '../components/MainFeaturedPost';
+// import { useParams } from 'react-router-dom';
 // import Sidebar from './Sidebar';
-// import Footer from './Footer';
+import { useSearchParams } from 'react-router-dom';
 
-const EventDetails: React.FC = () => {
-  const { getEventDetails } = useContext(EventContext);
-  const eventData = getEventDetails('Z7r9jZ1AdFYep');
+interface EventDetailsProps {
+  id: string;
+}
+const EventDetails: React.FC<EventDetailsProps> = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const mainFeaturedPost = {
-    title: eventData?.name,
-    description: `${eventData?.venues.name}... ${eventData?.venues.city}, ${eventData?.venues.state}`,
-    image: eventData?.image.url,
+  const idEvent = searchParams.get('id');
+
+  console.log('THIS IS ID!!!!!', idEvent);
+  const { getEventDetails, eventDetails } = useContext(EventContext);
+  useEffect(() => {
+    getEventDetails(idEvent);
+  }, []);
+
+  const mainFeaturedPost: {
+    description?: string;
+    image?: string;
+    title?: string;
+  } = {
+    title: eventDetails?.name,
+    description: `${eventDetails?.venues.name}... ${eventDetails?.venues.city.name}, ${eventDetails?.venues.state.name}`,
+    image: eventDetails?.image,
   };
+
+  // const description = {
+  //   description?: string;
+  // } = {
+  //   description:
+  // }
 
   const theme = createTheme();
 
   return (
+    // <div>{eventDetails?.name}</div>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Container maxWidth='lg'>
         <main>
           <MainFeaturedPost post={mainFeaturedPost} />
-          <Grid container spacing={5} sx={{ mt: 3 }}></Grid>
+          <Grid container spacing={5} sx={{ mt: 3 }}>
+            <Main
+              title='Event Details'
+              description={`${eventDetails?.dates.localDate}\n ${eventDetails?.dates.localTime}\n ${eventDetails?.dates.localDate}\n ${eventDetails?.venues.name}`}
+            />
+            {/* <Sidebar
+              title={sidebar.title}
+              description={sidebar.description}
+              archives={sidebar.archives}
+              social={sidebar.social}
+            /> */}
+          </Grid>
         </main>
       </Container>
     </ThemeProvider>
