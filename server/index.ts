bc82fa84909e5a3b25e60a2bc23584b9ae61dded
b.chau <brianchau7979@gmail.com>
2022-08-16 11:53:40 -0500

    changed google callback url from localhost to just auth/google/callback endpoint

--- server/index.ts
+++ server/index.ts
@@ -40,8 +40,7 @@ passport.use(new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:5000/auth/google/callback',
-    // userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
   },
   (async (req, accessToken, refreshToken, profile, cb ) => {
     // console.log('profile here----', profile);
