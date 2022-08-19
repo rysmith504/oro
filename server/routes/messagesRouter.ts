ed39b97016660790135303a216afa242e97f8318
rysmith504 <rysmith504@gmail.com>
2022-08-19 13:49:45 -0500

    removed function from router

--- server/routes/messagesRouter.ts
+++ server/routes/messagesRouter.ts
@@ -9,20 +9,5 @@ const messagesRouter = Router();
 messagesRouter.post('/addmsg', addMessage);
 messagesRouter.post('getmsg', getAllMessages);
 
-messagesRouter.get('/allusers', async (req, res, next) => {
-  const id = req.params.id;
-  try{
-    const users = await prisma.users.findMany({
-      where: {
-        id: {
-          not: req.params.id,
-        }
-      }
-    });
-    return res.json(users);
-  } catch (ex) {
-    next(ex);
-  }
-})
 
 export default messagesRouter;
