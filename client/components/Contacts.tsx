c73ce6e74ec8e5fe2b6e95c0e486ef3937f9d899
rysmith504 <rysmith504@gmail.com>
2022-08-19 09:45:41 -0500

    styled, built some front end and back end routes for chat

---
+++ client/components/Contacts.tsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useState, useContext } from 'react';
+
+import { UserContext } from '../context/UserContext';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+
+const Contacts = () => {
+  const userContext = useContext(UserContext);
+  const { currentUserInfo, userContacts } = userContext;
+  const currentUser = currentUserInfo;
+  const userContext = useContext(UserContext);
+  const { currentUserInfo, userContacts } = userContext;
+  const [currentUserName, setCurrentUserName ] = useState(undefined)
+  const [ currentUserImage, setCurrentUserImage ] = useState(undefined)
+  const [ currentSelected, setCurrentSelected ] = useState(undefined)
+  const [selectedIndex, setSelectedIndex] = useState(undefined);
+  useEffect(() => {
+    console.log('USERCONTACTS', userContacts, 'USER', currentUser)
+    if(currentUser) {
+      setCurrentUserName(currentUser.fullName);
+      setCurrentUserImage(currentUser.profileURL);
+    }
+  }, [currentUser])
+  const changeCurrentChat = (index, contact) => {
+    setCurrentSelected(index);
+    // changeChat(contact);
+  };
+
+  const handleListItemClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    index: number,
+  ) => {
+    setSelectedIndex(index);
+  };
+
+  return (
+    <div>
+      {/* {currentUserImage && currentUserName && (
+        <Container>
+          <div className="brand">
+            <h3>orochat</h3>
+          </div>
+          <div className='contacts'>
+            {
+              userContacts.map((contact, index) => {
+                return (
+                  <div className={`contact ${index === currentSelected ? 'selected': '' 
+                  }`}
+                  key={contact.id}
+                  >
+                    <div className='avatar'>
+                      <img src={contact.profileURL} />
+                    </div>
+                    <div className='username'>
+                      <h3>{contact.fullName}</h3>
+                    </div>
+                  </div>
+                )
+              })}
+          </div>
+        </Container>
+        
+      
+
+      )} */}
+      <div>
+      </div>
+      {
+        userContacts.map((contact, index) => {
+          return(
+          <List key={'list'+index}>
+          <ListItemButton 
+            key={'listitembutton'+index}
+            alignItems="flex-start"
+            selected={selectedIndex === index}
+            onClick={(event) => handleListItemClick(event, index)}
+          >
+              <ListItemAvatar key={'listitemavatar'+index}>
+                <Avatar key={'avatar'+index} src={contact.profileURL} />
+              </ListItemAvatar>
+              <ListItemText
+                primary={contact.fullName}
+                secondary={
+                  <React.Fragment>
+                    <Typography
+                      key={'contactTypography'+index}
+                      sx={{ display: 'inline'}
+                      variant="body2"
+                      color="#F3F3F3"
+                    >
+                    messages
+                    </Typography>
+                  </React.Fragment>
+                }
+                />
+            </ListItemButton>
+            <Divider key={'divider'+index} variant="inset" component="li" />
+          </List>
+            )})
+          }
+      </div>
+  )
+}
+
+export default Contacts;
---
+++ client/pages/UserChat.tsx
@@ -0,0 +1,111 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../context/UserContext';
+import styled from 'styled-components';
+import axios from 'axios';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Toolbar from '@mui/material/Toolbar';
+import Contacts from '../components/Contacts';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import IconButton from '@mui/material/IconButton';
+import RateReviewIcon from '@mui/icons-material/RateReview';
+import ChatContainer from '../components/ChatContainer'
+
+// const Container = styled.div`
+//   height: 100vh;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   gap: 1rem;
+//   align-items: center;
+//   background-color: #131324;
+//   .container {
+//     height: 85vh;
+//     width: 85vw;
+//     background-color: #00000076;
+//     grid-template-columns: 25% 75%;
+//     @media screen and (min-width:360px) and (max-width:480 px) {
+//       grid-template-columns: 35% 65%;
+//     }
+//   }
+// `;
+
+const UserChat: React.FC = () => {
+  const userContext = useContext(UserContext);
+  const { currentUserInfo, userContacts } = userContext;
+  const currentUser = currentUserInfo;
+  const navigate = useNavigate();
+  useEffect(() => {
+    console.log('USERCNTACTS CHAT', userContacts);
+    console.log('currentuser chat', currentUserInfo)
+  }, [])
+  // useEffect(() => {
+  //   if (!currentUserInfo.id) {
+  //     navigate('/login');
+  //   // } else if(currentUser) {
+  //   //   setCurrentUser(currentUser);
+  //   //   setContacts(userContacts)
+  //   // }
+  //   // if(currentUser){
+  //   //   setUserImage(currentUser.profileURL)
+  //   }
+  // }, [currentUserInfo]);
+  
+
+  //WORKING LOGIN REDIRECT
+  // useEffect(() => {
+  //   if (!currentUserInfo.id) {
+  //     navigate('/login');
+  //     }
+  //   }, [currentUserInfo]);
+
+
+  return (
+    <div>
+      <div>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                href='/profile'
+                sx={{ mr: 2 }}
+                >
+                <ArrowBackIosNewIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                {currentUser.fullName}
+              </Typography>
+              <Button color="inherit">< RateReviewIcon /></Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      </div>
+        <React.Fragment>
+          <Grid container columns={2} columnSpacing={0}>
+          <Grid item xs={1} key='contactscontainer' maxWidth="sm">
+            <Box sx={{ float: 'left', bgcolor: '#0D1013', height: 'auto', width: 'flex' }}>
+                <Contacts  key='contacts' />
+            </Box>
+          </Grid>
+          <Grid item xs={1} key='chatcontainer' maxWidth='sm'>
+            <Box sx={{float: 'right', bgcolor: '#0D1013', height: 'auto', width: 'flex' }}>
+              <ChatContainer />
+            </Box>
+          </Grid>
+          </Grid>
+    </React.Fragment>
+    </div>
+  );
+}
+
+
+export default UserChat;
