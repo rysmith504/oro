c22d6a2944f0a6630f152492edbcee2c3321ac0f
dow2 <dowedwards@gmail.com>
2022-08-20 10:24:31 -0500

    Backpack re worked (#112)
    * merging backpack
    
    * fixing files
    
    * merging with upstream
    
    * reworked budget feature

--- client/pages/BackPack.tsx
+++ client/pages/BackPack.tsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useContext } from 'react';
 import { UserContext } from '../context/UserContext';
-import Accordion from '@mui/material/Accordion';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import AccordionSummary from '@mui/material/AccordionSummary';
 import { styled } from '@mui/material/styles';
 import {
   ArrowForwardIosSharpIcon,
@@ -15,55 +12,64 @@ import {
   Button,
 } from '../styles/material';
 import BudgetItem from './BudgetItem';
-import { useTheme } from '@mui/material/styles';
-// const Accordion = styled((props) => (
-//   <MuiAccordion children={''} disableGutters elevation={0} square {...props} />
-// ))(({ theme }) => ({
-//   border: `1px solid ${theme.palette.divider}`,
-//   '&:not(:last-child)': {
-//     borderBottom: 0,
-//   },
-//   '&:before': {
-//     display: 'none',
-//   },
-// }));
+import axios from 'axios';
 
-// const AccordionSummary = styled((props) => (
-//   <MuiAccordionSummary
-//     expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
-//     {...props}
-//   />
-// ))(({ theme }) => ({
-//   backgroundColor:
-//     theme.palette.mode === 'dark'
-//       ? 'rgba(255, 255, 255, .05)'
-//       : 'rgba(0, 0, 0, .03)',
-//   flexDirection: 'row-reverse',
-//   '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
-//     transform: 'rotate(90deg)',
-//   },
-//   '& .MuiAccordionSummary-content': {
-//     marginLeft: theme.spacing(1),
-//   },
-// }));
+const Accordion = styled((props) => (
+  <MuiAccordion children={''} disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  '&:not(:last-child)': {
+    borderBottom: 0,
+  },
+  '&:before': {
+    display: 'none',
+  },
+}));
 
-// const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-//   padding: theme.spacing(2),
-//   borderTop: '1px solid rgba(0, 0, 0, .125)',
-// }));
+const AccordionSummary = styled((props) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark'
+      ? 'rgba(255, 255, 255, .05)'
+      : 'rgba(0, 0, 0, .03)',
+  flexDirection: 'row-reverse',
+  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    transform: 'rotate(90deg)',
+  },
+  '& .MuiAccordionSummary-content': {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: '1px solid rgba(0, 0, 0, .125)',
+}));
+
+const SAMPLE_BUDGET_LIST = [
+  { id: 1, name: 'Tickets' },
+  { id: 2, name: 'Food' },
+  { id: 3, name: 'Drinks' },
+  { id: 4, name: 'Parking' },
+  { id: 5, name: 'Merch' },
+  { id: 6, name: 'Travel' },
+];
+
+const formatCurrency = (number) => {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
 
 const BackPack: React.FC = () => {
   const { userEvents, getUserEvents, currentUserInfo } =
     useContext(UserContext);
   const [expanded, setExpanded] = React.useState('panel1');
 
-  const theme = useTheme();
-  const iconColors = theme.palette.secondary.contrastText;
-  const inverseMode = theme.palette.secondary.main;
+  const [budgetList, setBudgetList] = React.useState([...SAMPLE_BUDGET_LIST]);
 
-  // <YouTubeIcon key={'youtube'} sx={{ color: iconColors }} />
-  // <CardContent sx={{ bgcolor: inverseMode }}></CardContent>
-  // <Typography paragraph sx={{ bgcolor: inverseMode }}></Typography>
   const handleChange = (panel) => (event, newExpanded) => {
     setExpanded(newExpanded ? panel : false);
   };
@@ -72,25 +78,54 @@ const BackPack: React.FC = () => {
     getUserEvents();
   }, []);
 
+  const handleBudgetChange = (value, index) => {
+    console.log({ value, index });
+    let newList = [...budgetList];
+    newList[index]['value'] = parseInt(value, 10);
+    setBudgetList(newList);
+  };
+
+  let totalSum = budgetList.reduce((prev, curr) => ( prev + (curr?.value ?? 0) ),0));
+
+  console.log({ userEvents, totalSum });
+
+  const handleSubmit = async () => { 
+    const url = `api/eventa/budgetsubmit`;
+
+    try { 
+      axios.post(
+        url, budgetList 
+      ).then(resp => console.log("Successful budget"))
+      .catch(err => console.log(err))
+
+    } catch(err){ console.log(err)}
+  }
+
   return (
     <Accordion
-      // expanded={expanded === 'panel1'}
+      expanded={expanded === 'panel1'}
       onChange={handleChange('panel1')}
-      sx={{ bgcolor: inverseMode, mt: '40px' }}
     >
-      <AccordionSummary
-        sx={{ bgcolor: inverseMode }}
-        aria-controls='panel1d-content' id='panel1d-header'>
+      <AccordionSummary aria-controls='panel1d-content' id='panel1d-header'>
         <Typography>{userEvents.eventName}</Typography>
         <Typography>{userEvents.eventDate}</Typography>
       </AccordionSummary>
-      <AccordionDetails sx={{ color: inverseMode }}>
-        <BudgetItem sx={{ color: inverseMode }} eventId={userEvents.eventId} label='Tickets' />
-        <BudgetItem sx={{ color: inverseMode }} eventId={userEvents.eventId} label='Food' />
-        <BudgetItem sx={{ color: inverseMode }} eventId={userEvents.eventId} label='Drinks' />
-        <BudgetItem sx={{ color: inverseMode }} eventId={userEvents.eventId} label='Parking' />
-        <BudgetItem sx={{ color: inverseMode }} eventId={userEvents.eventId} label='Merch' />
-        <BudgetItem sx={{ color: inverseMode }} eventId={userEvents.eventId} label='Travel' />
+      <AccordionDetails>
+
+        {budgetList?.map((item, index) => {
+          return (
+            <BudgetItem
+              key={index}
+              label={item.name}
+              eventId={item.id}
+              value={item.value}
+              onChange={(e) => handleBudgetChange(e.target.value, index)}
+            />
+          );
+        })}
+
+        <div>Total: ${totalSum ? formatCurrency(totalSum) : 0}</div>
+        <Button onClick={handleSubmit}>Submit Budget</Button>
       </AccordionDetails>
     </Accordion>
   );
--- client/pages/BudgetItem.tsx
+++ client/pages/BudgetItem.tsx
@@ -1,49 +1,27 @@
 /* eslint-disable func-style */
-import { Button, Checkbox, List, ListItem, TextField, Typography } from '@mui/material';
+import { Checkbox, List, ListItem, TextField } from '@mui/material';
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useTheme } from '@mui/material/styles';
-function BudgetItem(props: any) {
-  const theme = useTheme();
-  const iconColors = theme.palette.secondary.contrastText;
-  const inverseMode = theme.palette.secondary.main;
 
-  // <YouTubeIcon key={'youtube'} sx={{ color: iconColors }} />
-  // <CardContent sx={{ bgcolor: inverseMode }}></CardContent>
-  // <Typography paragraph sx={{ bgcolor: inverseMode }}></Typography>
+type BudgetItemProps = {
+  label: string;
+  value?: number;
+  onChange?: () => void;
+};
 
-  const [formData, setFormData] = useState({
-    itemName: props.label,
-    budget: 0,
-    event_id: 0,
-    customer_id: 0,
-  });
-
-  const handleBudgetSubmit = async () => {
-    formData.event_id = props.eventId;
-    axios
-      .post('/api/events/budgetsubmit', formData)
-      .then((events) => {
-        console.log('Successfully submitted budget');
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
+function BudgetItem({ label, value, onChange }: BudgetItemProps): JSX.Element {
   return (
     <>
       <List>
         <ListItem>
+          <span>{label}</span>
           <TextField
-            sx={{ bgcolor: iconColors, color: iconColors, mr: '20px' }}
-            label={`Enter your ${props.label} budget`}
-            onChange={(e) =>
-              setFormData({ ...formData, budget: e.target.value })
-            }
+            label={`Enter your budget ${label}?`}
+            variant='outlined'
+            value={value}
+            onChange={onChange}
           />
-          <Button
-            sx={{ bgcolor: iconColors, color: inverseMode }}
-            onClick={handleBudgetSubmit}>Submit Budget</Button>
+          <span>: ${value ?? 0}</span>
         </ListItem>
       </List>
     </>
