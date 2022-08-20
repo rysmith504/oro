fb8e9589e06ef08d73a08f50c7bafaecd9e45a63
b.chau <brianchau7979@gmail.com>
2022-08-20 11:23:32 -0500

    attempting to get all userEvents

--- server/routes/profile.ts
+++ server/routes/profile.ts
@@ -6,24 +6,34 @@ import prisma from '../database/db';
 const profileRouter = Router();
 const TICKETMASTER_API_KEY = process.env.TICKETMASTER_API_KEY;
 
-profileRouter.get('/events', (req, res) => {
-  prisma.userEvents.findMany({ where: { userId: 1 }})
-    .then((userEvents) => {
-      const eventId = userEvents[0]?.eventAPIid;
 
-      axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${TICKETMASTER_API_KEY}&id=${eventId}`)
-        .then(event => {
-          res.status(200).send(event.data._embedded.events[0]);
-        })
-        .catch(err => {
-          console.error(err);
-        });
-
-    })
-    .catch((err) => {
-      res.send(500);
-    });
-});
+// profileRouter.get('/events/:_id', (req, res) => {
+//   const { _id } = req.params;
+//   let userEvents = [];
+//   prisma.users.findUnique({ where: { googleId: _id }})
+//     .then((user) => {
+//       const { id } = user;
+//       return id;
+//     })
+//     .then((id => {
+//       prisma.userEvents.findMany({ where: { userId: id }})
+//         .then((events) => {
+//           events.forEach(event => {
+//             axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${TICKETMASTER_API_KEY}&id=${event.eventAPIid}`)
+//               .then(({data}) => {
+//                 const { _embedded } = data;
+//                 userEvents.push(_embedded.events);
+//               })
+//               .catch(err => console.error(err));
+//           })
+//         })
+//         .then(() => {
+//           console.log(userEvents);
+//         })
+//         .catch(err => console.error(err));
+//     }))
+//     .catch(err => console.error(err));
+// });
 
 profileRouter.get('/:_id', (req, res) => {
   const { _id } = req.params;
