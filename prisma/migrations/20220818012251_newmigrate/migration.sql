6c3d726f93b1d640652cf0aefcce0628d741f4a4
dow2 <dowedwards@gmail.com>
2022-08-26 12:51:31 -0500

    rendering different budgets for each event

--- prisma/migrations/20220818012251_newmigrate/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Comments" DROP CONSTRAINT "Comments_userId_fkey";
-
--- AlterTable
-ALTER TABLE "Comments" ALTER COLUMN "userId" SET DATA TYPE TEXT;
-
--- AddForeignKey
-ALTER TABLE "Comments" ADD CONSTRAINT "Comments_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/migrations/20220824181422_artist_name_unique_identifier/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[artistName]` on the table `ArtistFollowing` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "ArtistFollowing_artistName_key" ON "ArtistFollowing"("artistName");
