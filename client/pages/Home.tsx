cc1e5d0efe64e279d13dea6798c3e83092bcc537
Bethany Jones <betpetjones@gmail.com>
2022-08-28 14:46:06 -0500

    Added icons to home page (#152)
    * typescript resolved in artist and theme and nav
    
    * more typescript updates, also push pins now coloring
    
    * more typescript in contacts
    
    * added icons to home page

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -1,17 +1,76 @@
-import React, { useEffect } from 'react';
-import {Box, Grid } from '../styles/material';
+import React, { useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { ThemeContext } from '../context/ThemeContext';
 
+import { Box, Grid, UseTheme, Typography, Badge, TravelExploreIcon, MusicNoteIcon, LuggageIcon, GradeIcon, PriceChangeIcon, ForumIcon, EmailIcon, AccountCircleIcon } from '../styles/material';
+
+import { useNavigate } from 'react-router-dom';
 
 const Home: React.FC = () => {
+  const theme = UseTheme();
+  const iconColors = theme.palette.secondary.contrastText;
+  const inverseMode = theme.palette.secondary.main;
+  const themeContext = useContext(ThemeContext);
+  const { mode, toggleMode } = themeContext;
+  const navigate = useNavigate();
   useEffect(() => {
     // getUser();
   }, []);
-
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1, mt: '60px' }}>
       <Grid container spacing={2}>
-        <Grid xs={12} md={12} item>
-          <img src="/images/concert.jpg" width="50%"/>
+        <Grid xs={6} md={6} item sx={{ mb: '15px' }}>
+        <Link
+      to='/eventListings'
+      style={{ textDecoration: 'none' }}
+      key={'eventListings'}
+    >
+      <TravelExploreIcon sx={{ color: inverseMode, fontSize: '4.5rem' }} className='nav-icons'/>
+      <Typography sx={{ color: inverseMode }}>Find Events</Typography>
+    </Link>
+        </Grid>
+        <Grid xs={6} md={6} item sx={{ mb: '15px' }}>
+        <Link to='/songFinder' style={{ textDecoration: 'none' }} key={'songFinder'}>
+      <MusicNoteIcon sx={{ color: inverseMode,  fontSize: '4.5rem' }} className='nav-icons'/>
+      <Typography sx={{ color: inverseMode }}>Song Finder</Typography>
+    </Link>
+        </Grid>
+        <Grid xs={6} md={6} item sx={{ mb: '15px' }}>
+        <Link to='/artists' style={{ textDecoration: 'none' }} key={'artists'}>
+      <GradeIcon sx={{ color: inverseMode,  fontSize: '4.5rem' }} className='nav-icons'/>
+      <Typography sx={{ color: inverseMode }}>Favorite Artists</Typography>
+    </Link>
+        </Grid>
+        <Grid xs={6} md={6} item sx={{ mb: '15px' }}>
+        <Link to='/chat' style={{ textDecoration: 'none' }} key={'chat'}> <ForumIcon sx={{ color: inverseMode,  fontSize: '4.5rem' }} className='nav-icons'/><Typography sx={{ color: inverseMode }}>Chat</Typography></Link>
+        </Grid>
+        <Grid xs={6} md={6} item sx={{ mb: '15px' }}>
+        <Link to='/profile' style={{ textDecoration: 'none' }} key={'profile'}>
+        <AccountCircleIcon sx={{ color: inverseMode,  fontSize: '4.5rem' }}/>
+      <Typography sx={{ color: inverseMode }}>Account</Typography></Link>
+        </Grid>
+        <Grid xs={6} md={6} item sx={{ mb: '15px' }}>
+        <Link to='/notifications' style={{ textDecoration: 'none' }} key={'notifications'}>
+        <Badge color="primary" >
+          <EmailIcon sx={{ color: inverseMode,  fontSize: '4.5rem' }} className='nav-icons'/>
+        </Badge>
+        <Typography sx={{ color: inverseMode }}>Notifications</Typography></Link>
+        </Grid>
+        <Grid xs={6} md={6} item sx={{ mb: '15px' }}>
+        <Link to='/backpack' style={{ textDecoration: 'none' }} key={'backpack'}>
+      <PriceChangeIcon sx={{ color: inverseMode,  fontSize: '4.5rem' }} className='nav-icons'/>
+      <Typography sx={{ color: inverseMode }}>Budgets</Typography>
+    </Link>
+        </Grid>
+        <Grid xs={6} md={6} item sx={{ mb: '15px' }}>
+        <Link
+      to='/travelPlanner'
+      style={{ textDecoration: 'none' }}
+      key={'travelPlanner'}
+    >
+      <LuggageIcon className='nav-icons' sx={{ color: inverseMode,  fontSize: '4.5rem' }}/>
+      <Typography sx={{ color: inverseMode }}>Travel Planner</Typography>
+    </Link>
         </Grid>
       </Grid>
     </Box>
